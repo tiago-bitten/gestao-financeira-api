209bf0a5ee7048f323e19545c8ec655428715e07
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-11-29 00:31:54 -0300

    inital files

---
+++ src/main/java/com/labisistemas/gestaofinanceiraapi/GestaoFinanceiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.labisistemas.gestaofinanceiraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFinanceiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFinanceiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labisistemas/gestaofinanceiraapi/GestaoFinanceiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labisistemas.gestaofinanceiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFinanceiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
