7b99911516958933893f349fc46d5e2e4e30d172
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-11-29 21:56:44 -0300

    refactor: add validation to CreateUserDto dto

--- src/main/java/com/labisistemas/gestaofinanceiraapi/dto/CreateUserDto.java
+++ src/main/java/com/labisistemas/gestaofinanceiraapi/dto/CreateUserDto.java
@@ -1,8 +1,16 @@
 package com.labisistemas.gestaofinanceiraapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateUserDto(
+
+        @NotBlank
         String name,
+
+        @NotBlank
         String email,
+
+        @NotBlank
         String password
 ) {
 }
