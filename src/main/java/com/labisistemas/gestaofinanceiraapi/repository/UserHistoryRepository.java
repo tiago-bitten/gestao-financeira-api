25ec330191c5ddc28daa2d38b77edc65df47f7db
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-11-29 21:32:05 -0300

    feat: add UserHistoryRepository repository

--- src/main/java/com/labisistemas/gestaofinanceiraapi/repository/UserHistoryRepository.java
+++ src/main/java/com/labisistemas/gestaofinanceiraapi/repository/UserHistoryRepository.java
@@ -1,2 +1,8 @@
-package com.labisistemas.gestaofinanceiraapi.repository;public interface UserHistoryRepository {
+package com.labisistemas.gestaofinanceiraapi.repository;
+
+import com.labisistemas.gestaofinanceiraapi.enterprise.CustomQuerydslPredicateExecutor;
+import com.labisistemas.gestaofinanceiraapi.model.UserHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserHistoryRepository extends JpaRepository<UserHistory, Long>, CustomQuerydslPredicateExecutor<UserHistory> {
 }
