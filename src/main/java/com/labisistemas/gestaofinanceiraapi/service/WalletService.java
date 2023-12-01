6ea5f1093af8aace05c41c87695aa1de23ec2c5f
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-01 20:30:07 -0300

    feat: add create wallet method

---
+++ src/main/java/com/labisistemas/gestaofinanceiraapi/service/WalletService.java
@@ -0,0 +1,25 @@
+package com.labisistemas.gestaofinanceiraapi.service;
+
+import com.labisistemas.gestaofinanceiraapi.model.Wallet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    public ReadWalletDto create(CreateWalletDto dto) {
+        walletRepository.findByName(dto.name())
+                .ifPresent(wallet -> {
+                    throw new ValidationException("Wallet already exists");
+                });
+
+        Wallet wallet = new Wallet(dto.name(), dto.description(), dto.balance(), dto.user());
+
+        walletRepository.save(wallet);
+
+        return new ReadWalletDto(wallet.getId(), wallet.getName(), wallet.getDescription(), wallet.getBalance(), wallet.getCurrency(), wallet.getUser().getId());
+    }
+}
