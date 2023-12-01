d8bacadeb23ed60f3a954b5d78454633c88cd874
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-01 20:25:01 -0300

    ..

--- src/main/java/com/labisistemas/gestaofinanceiraapi/dto/ReadUserHistoriesDto.java
+++ src/main/java/com/labisistemas/gestaofinanceiraapi/dto/ReadUserHistoriesDto.java
@@ -1,2 +1,16 @@
-package com.labisistemas.gestaofinanceiraapi.dto;public record ReadUserHistoriesDto() {
+package com.labisistemas.gestaofinanceiraapi.dto;
+
+import com.labisistemas.gestaofinanceiraapi.enums.ChangeType;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public record ReadUserHistoriesDto(
+        Long id,
+        String name,
+        String email,
+        Long userId,
+        ChangeType changeType,
+        Instant changedAt
+) {
 }
--- src/main/java/com/labisistemas/gestaofinanceiraapi/service/UserHistoryService.java
+++ src/main/java/com/labisistemas/gestaofinanceiraapi/service/UserHistoryService.java
@@ -1,10 +1,13 @@
 package com.labisistemas.gestaofinanceiraapi.service;
 
+import com.labisistemas.gestaofinanceiraapi.dto.ReadUserHistoriesDto;
 import com.labisistemas.gestaofinanceiraapi.enums.ChangeType;
 import com.labisistemas.gestaofinanceiraapi.model.User;
 import com.labisistemas.gestaofinanceiraapi.model.UserHistory;
 import com.labisistemas.gestaofinanceiraapi.repository.UserHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +19,10 @@ public class UserHistoryService {
     public void log(User user, ChangeType changeType) {
         UserHistory userHistory = new UserHistory(user.getName(), user.getEmail(), user.getPassword(), changeType, user);
         userHistoryRepository.save(userHistory);
+    }
+
+    public Page<ReadUserHistoriesDto> findAll(String filter, Pageable pageable) {
+        Page<UserHistory> userHistories = userHistoryRepository.findAll(filter, UserHistory.class, pageable);
+        return userHistories.map(userHistory -> new ReadUserHistoriesDto(userHistory.getId(), userHistory.getName(), userHistory.getEmail(), userHistory.getUser().getId(), userHistory.getChangeType(), userHistory.getChangedAt()));
     }
 }
